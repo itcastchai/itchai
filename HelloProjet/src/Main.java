b5c97f3732b1354f5a4751615487513fa8a67540
xiaoxuanfeng <aa@xuanfeng>
2020-02-19 13:19:42 +0800

    提交2

--- HelloProjet/src/Main.java
+++ HelloProjet/src/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
     @Test
     public void testHelloWrold() {
-        System.out.println("666");
+        System.out.println("999");
         System.out.println("bbb");
         System.out.println("HelloWorld!");
     }
